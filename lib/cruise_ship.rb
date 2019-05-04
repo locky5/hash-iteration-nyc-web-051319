73905589dc90d221e6fc6ba10efd5eb8c622350b
Kevin Dai <daikevin19@yahoo.com>
2019-05-04 04:48:28 +0000

    Done.

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -7,5 +7,11 @@
 # }
 
 def select_winner(passengers)
-  # add the code snippet here!
+  winner = ""
+  passengers.each do |suite, name| ##iterates through hash
+    if suite == :suite_a && name.start_with?("A")
+      winner = name ##saves variable to be returned later
+    end
+  end
+  winner 
 end
